d2225a59684e5f73c6b84abf4913004a36885de9
bellandry <bellandry.work@gmail.com>
2024-09-28 21:45:47 +0100

    add localStorage

--- scripts.js
+++ scripts.js
@@ -8,8 +8,7 @@ const taskList = document.getElementById("taskList");
 //écoute de l'événement de clic sur le bouton
 addTaskBtn.addEventListener("click", addTask);
 
-//tableau contenant les taches
-let tasks = [];
+window.addEventListener("load", displayTasks);
 
 // fonction pour ajouter les taches
 function addTask() {
@@ -17,25 +16,46 @@ function addTask() {
   const task = taskInput.value;
   // si le texte n'est pas vide alors on peut l'enregistrer
   if (task) {
+    let tasks = getTasks();
     //ajouter la tache dans le tableau des taches
     tasks.push(task);
     // vider l'input
     taskInput.value = "";
+    saveTask(tasks);
     //actualiser la liste des taches
     displayTasks();
   }
 }
 
-function getTasks() {}
+function saveTask(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTasks() {
+  let tasks = localStorage.getItem("tasks");
+
+  if (tasks) {
+    return JSON.parse(tasks);
+  } else {
+    return [];
+  }
+
+  // version racourcis de la condition
+  // return tasks ? JSON.pasrse(tasks) : []
+}
 
 function deleteTask(index) {
+  let tasks = getTasks();
   tasks.splice(index, 1);
+  saveTask(tasks);
   displayTasks();
 }
 
 function displayTasks() {
   //vider l'affichage des taches
   taskList.innerHTML = "";
+
+  let tasks = getTasks();
   //parcourir le tableau des taches
   tasks.forEach((task, index) => {
     // créer la balise li pour chaque tache
